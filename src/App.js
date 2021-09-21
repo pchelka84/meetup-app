c8bbaf2021574340450d1bd884814f69ed1dde43
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-21 14:12:23 -0400

    Create and style main navigation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
         </Route>
+
         <Route path='/new-meetup'>
           <NewMeetupPage />
         </Route>
