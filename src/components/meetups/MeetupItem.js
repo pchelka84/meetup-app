e3e59bcb00cece30b1a3d7ea04170f3d7b044ff4
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-21 14:51:35 -0400

    Creating card wrapper component

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
+import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   const { image, title, address, description } = props.meetup;
   return (
     <li className={classes.item}>
-      <div>
-        <img src={image} alt={title} className={classes.image} />
-      </div>
-      <div className={classes.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
